a4f57cc3d903ca653a1cd18ef368b01fe2eb128b
yukendhiran <yukendhiranelango@gmail.com>
2024-09-15 12:56:33 +0530

    Chapter 2.9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	
 )
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
