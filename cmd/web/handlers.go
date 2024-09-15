3608e2e488fd622448e5b2fa4201e7d2cc48af03
yukendhiran <yukendhiranelango@gmail.com>
2024-09-15 12:43:28 +0530

    Chapter 2.8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 func home(w http.ResponseWriter, r *http.Request) {
-  w.Header().Add("Server", "Go")
-  w.Write([]byte("Hello from Snippetbox"))
+	w.Header().Add("Server", "Go")
+	// Include the navigation partial in the template files.
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
