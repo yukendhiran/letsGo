5e6f6a6e3ea8fc393be7305a23dd55324e72dc3e
yukendhiran <yukendhiranelango@gmail.com>
2024-09-16 08:46:20 +0530

    Chapter 3.4

---
+++ cmd/web/helper.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri = r.URL.RequestURI()
+		//trace = string(debug.Stack())
+	)
+	//app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace) Let's say I dont want stack trace for now
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
