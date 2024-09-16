253596cbd46b11ba5404da846e0c08519f3d20f0
yukendhiran <yukendhiranelango@gmail.com>
2024-09-16 08:49:28 +0530

    Chapter 3.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,17 +20,9 @@ func main() {
 
 	app := &application{logger: logger}
 
-	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 	logger.Info("starting server", slog.String("addr", *addr))
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
