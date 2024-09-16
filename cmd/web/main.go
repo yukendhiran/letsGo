815027f3dfc767b83497480098eb667172db5a79
yukendhiran <yukendhiranelango@gmail.com>
2024-09-16 07:56:58 +0530

    Chapter 3.1

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -14,7 +18,7 @@ func main() {
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
